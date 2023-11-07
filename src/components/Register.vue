274c9754f3834ec29f5addcb100678c83d769ee6
birruhayyu <birru.hayyu@gmail.com>
2023-11-07 18:15:37 +0100

    first commit

---
+++ src/components/Register.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+  <v-container>
+  <v-text-field
+      label="Email"
+      :rules="rules"
+      hide-details="auto"
+    ></v-text-field>
+    <v-text-field label="Password"></v-text-field>
+  <v-btn>Submit</v-btn>
+  </v-container>
+</template>
+<script setup>
+</script>
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <Register />
+</template>
+
+<script setup>
+  // import HelloWorld from '@/components/HelloWorld.vue';
+  import Register from '@/components/Register.vue';
+</script>
